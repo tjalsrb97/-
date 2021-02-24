3ea4330a28292c9456482c7d57784eb6239ed095
tjalsrb97 <gnam011@naver.com>
2021-02-24 13:17:16 +0900

    update

---
+++ 코드업/practice1000.py
@@ -0,0 +1,34 @@
+#1014
+# a, b = input().split()
+# print(b, a)
+
+#1015
+# n=float(input())
+# print("%.2f" % n)
+
+#1017
+# n=int(input())
+# print(n, n, n)
+
+#1018
+# a, b = input().split(":")
+# print('%s:%s' %(a,b))
+
+#1019
+# y, m, d = input().split(".")
+# print("%04d.%02d.%02d" %(int(y),int(m),int(d)))
+
+#1020
+# front, back = input().split("-")
+# print(front+back)
+
+#1021
+# input1 = str(input())
+# print(input1)
+
+#1022
+# import sys
+# data = sys.stdin.readline().rstrip()
+# print(data)
+
+#1023
